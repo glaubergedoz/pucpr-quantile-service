eb82d5dc0f17da1dc5bb1c853695156828920631
Glauber <glauber@gdoz.com.br>
2025-04-28 23:36:00 -0300

    add new unit tests

--- test/quantile_service/unit/add_sample_test.clj
+++ test/quantile_service/unit/add_sample_test.clj
@@ -9,3 +9,20 @@
                      :timestamp 1000}
           new-state (add-sample->state nil sample)]
       (is (= [sample] (:events new-state))))))
+
+(deftest add-sample-to-existing-state
+  (testing "add-sample->state appends new event to existing state"
+    (let [initial-sample {:value 1 :timestamp 100}
+          initial-state  (add-sample->state nil initial-sample)
+          second-sample  {:value 2 :timestamp 200}
+          new-state      (add-sample->state initial-state second-sample)]
+      (is (= [initial-sample second-sample]
+             (:events new-state))))))
+
+(deftest cms-count-increments
+  (testing "CountMinSketch in StateData increments for same value"
+    (let [sample {:value 5 :timestamp 100}
+          state1 (add-sample->state nil sample)
+          state2 (add-sample->state state1 sample)
+          cms    (:cms state2)]
+      (is (= 2 (.estimateCount cms "5"))))))
